747b29cf102bc67d15da9c66aaa1a17619c6283f
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-09 00:39:11 +0600

    first commit for

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold">
+        Welcome to <span className="text-amber-400">Neu</span>
+        <span className="text-rose-500">romancer</span>
+      </h1>
+    </main>
+  );
+};
+
+export default Home;
