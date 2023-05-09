31085ef9fa0b7cb8403c1de6c276fa03ad6f0a47
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-09 11:57:50 +0600

    add(index): created index for ui

---
+++ src/components/ui/index.ts
@@ -0,0 +1,9 @@
+export { default as NrButton } from "./NrButton";
+export { default as NrDjCard } from "./NrDjCard";
+export { default as NrEventCard } from "./NrEventCard";
+export { default as NrLinkButton } from "./NrLinkButton";
+export { default as NrList } from "./NrList";
+export { default as NrListItem } from "./NrListItem";
+export { default as NrSocialCard } from "./NrSocialCard";
+export { default as NrText } from "./NrText";
+export { default as NrTitle } from "./NrTitle";
