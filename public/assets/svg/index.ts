7248139d4bad42fb64b0666a0240dc452c5c81da
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-09 12:01:48 +0600

    assets(public): added new demo images, svg

---
+++ public/assets/svg/index.ts
@@ -0,0 +1,7 @@
+export { default as ArrowRightIcon } from "./ArrowRightIcon.svg";
+export { default as CalendarIcon } from "./CalendarIcon.svg";
+export { default as ConfettiBallIcon } from "./ConfettiBallIcon.svg";
+export { default as GameDieIcon } from "./GameDieIcon.svg";
+export { default as JigsawPuzzlePieceIcon } from "./JigsawPuzzlePieceIcon.svg";
+export { default as ListMarkerIcon } from "./ListMarkerIcon.svg";
+export { default as TicketIcon } from "./TicketIcon.svg";
