e1c5690b57de89cafeb53725e1b11fc6429f4a2b
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-12 14:33:13 +0600

    add(home-section): added HomeCorporate section for homepage

---
+++ src/components/sections/HomeCorporate.tsx
@@ -0,0 +1,51 @@
+import {
+  NrButton,
+  NrContainer,
+  NrEventCard,
+  NrText,
+  NrTitle,
+} from "@/components/ui";
+import { corporateData } from "@/mock/data";
+import { NrEvent } from "@/types/event";
+
+const HomeCorporate = () => {
+  return (
+    <section className="bg-home-corporate-bgi bg-home-corporate-bgs">
+      <NrContainer
+        styles={{
+          size: "md",
+          css: "flex flex-col gap-20 px-xl items-center lg:max-w-screen-xl",
+        }}
+      >
+        <div className="flex flex-col gap-lg max-w-screen-md m-auto">
+          <NrTitle
+            component="h2"
+            styles={{
+              align: "center",
+            }}
+          >
+            VR Corporate Events
+          </NrTitle>
+          <NrText
+            styles={{
+              align: "center",
+            }}
+          >
+            Elevate your corporate events with immersive VR games that unite
+            teams, enhance communication, foster strength, and drive sales
+            through engaging gameplay.
+          </NrText>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-10 md:flex-row lg:flex-col">
+          {corporateData.map((item, index) => (
+            <div key={index}>
+              <NrEventCard item={item as NrEvent} />
+            </div>
+          ))}
+        </div>
+      </NrContainer>
+    </section>
+  );
+};
+
+export default HomeCorporate;
