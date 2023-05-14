3b440f6b594c96b56acdbe6a1b7afd66a4e2858b
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-14 18:07:45 +0600

    add(mock): added mock data

---
+++ src/mock/data.ts
@@ -0,0 +1,593 @@
+export const eventsData = [
+  {
+    preset: "vr",
+    images: [
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    title: "The Galactic Circuit",
+    status: "Coming Soon",
+    descriptions:
+      "The Galactic Circuit is a weekly event that takes place in the VR Cinema 9D. It is a place where you can meet new people, play games, and watch movies.",
+
+    featured: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "VR Cinema 9D",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "All Ages Welcome",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "Every Wednesday",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "Walk-ins Allowed",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Games",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/",
+    },
+    share: {
+      enabled: true,
+    },
+  },
+];
+
+export const exploreData = [
+  {
+    uuid: "1",
+    preset: "vr",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/Vr9DCinemaIcon.svg",
+    title: "VR 9D Cinema 🔥",
+    status: "",
+    description:
+      "Experience boundless virtual worlds and events with real-time effects.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Starts $5",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "5 Years and Up",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "4 to 8 Minutes",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "In Events",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Multiplayer",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-9d-cinema",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "2",
+    preset: "vr",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrFreeRoamIcon.svg",
+    title: "VR Free Roam",
+    status: "",
+    description: "Go completely wireless and soul your VR gaming capabilities.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Starts $5",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "5 Years and Up",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "4 to 8 Minutes",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "In Events",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Multiplayer",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-free-roam",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "3",
+    preset: "vr",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrEscapeRoomIcon.svg",
+    title: "VR Escape Room",
+    status: "",
+    description:
+      "Enter a puzzle-solving adventure with your friends and family.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Starts $5",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "5 Years and Up",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "4 to 8 Minutes",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "In Events",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Multiplayer",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-escape-room",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "4",
+    preset: "vr",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrFreePlayIcon.svg",
+    title: "VR Free Play ⚡",
+    status: "",
+    description:
+      "Play over 70 engaging and exciting games that offer a unique type of fun.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Starts $5",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "5 Years and Up",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "4 to 8 Minutes",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "In Events",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Multiplayer",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "5",
+    preset: "vr",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrPartyIcon.svg",
+    title: "VR Party 🌠",
+    status: "",
+    description: "Great for birthdays, family gatherings, hangouts.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Starts $5",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "5 Years and Up",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "4 to 8 Minutes",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "In Events",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Multiplayer",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "6",
+    preset: "vr",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrDjIcon.svg",
+    title: "Bar 💄",
+    status: "",
+    description:
+      "Enjoy traditional beers, cocktails, and snacks in the heart of fun attractions.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Starts $5",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "5 Years and Up",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "4 to 8 Minutes",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "In Events",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Multiplayer",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+];
+
+export const corporateData = [
+  {
+    uuid: "1",
+    preset: "corporate",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/Vr9DCinemaIcon.svg",
+    title: "VR 9D Cinema 🔥",
+    status: "",
+    description:
+      "Experience boundless virtual worlds and events with real-time effects.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Starts $5",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "5 Years and Up",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "4 to 8 Minutes",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "In Events",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Multiplayer",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-9d-cinema",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "2",
+    preset: "corporate",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrFreeRoamIcon.svg",
+    title: "VR Free Roam",
+    status: "",
+    description: "Go completely wireless and soul your VR gaming capabilities.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Starts $5",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "5 Years and Up",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "4 to 8 Minutes",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "In Events",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Multiplayer",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-free-roam",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+];
+
+export const socialData = {
+  facebook: "https://www.facebook.com/neuromancer.vr",
+  instagram: "https://www.instagram.com/neuromancer.vr/",
+  twitter: "https://twitter.com/neuromancer_vr",
+  soundcloud: "https://soundcloud.com/neuromancer-vr",
+};
+
+export const galleriesData = [
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-1.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-2.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-3.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-4.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-5.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-6.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-7.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-8.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-9.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-10.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-11.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-12.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-13.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-14.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-15.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-16.jpg",
+    height: 0,
+    width: 0,
+  },
+  {
+    title: "Neuromancer",
+    src: "/assets/images/gallery-image-17.jpg",
+    height: 0,
+    width: 0,
+  },
+];
