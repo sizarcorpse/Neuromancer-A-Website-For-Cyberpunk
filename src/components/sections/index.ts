12e4ed8bee6fa575d7431a16d93f95c487d61bc8
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-14 18:07:05 +0600

    update(index): unpadted index

---
+++ src/components/sections/index.ts
@@ -0,0 +1,3 @@
+export { default as HomeCorporate } from "./HomeCorporate";
+export { default as HomeExplore } from "./HomeExplore";
+export { default as HomeHero } from "./HomeHero";
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,8 @@
 export { default as NrButton } from "./NrButton";
+export { default as NrComponentSlider } from "./NrComponentSlider";
 export { default as NrContainer } from "./NrContainer";
 export { default as NrDjCard } from "./NrDjCard";
+export { default as NrDropdown } from "./NrDropdown";
 export { default as NrEventCard } from "./NrEventCard";
 export { default as NrImageSlider } from "./NrImageSlider";
 export { default as NrLinkButton } from "./NrLinkButton";
