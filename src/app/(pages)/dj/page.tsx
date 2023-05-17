65ad9f268cf679ea46e2b4006e5bd962e79bf643
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-17 18:38:10 +0600

    test(festival-page): added temporary festivalpage

---
+++ src/app/(pages)/dj/page.tsx
@@ -0,0 +1,13 @@
+import { DjFeatured, DjHero, DjWhy } from "@/components/sections";
+
+const BarPage = () => {
+  return (
+    <>
+      <DjHero />
+      <DjWhy />
+      <DjFeatured />
+    </>
+  );
+};
+
+export default BarPage;
