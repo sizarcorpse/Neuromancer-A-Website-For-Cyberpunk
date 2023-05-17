a3dd184cd0cbb28a886b3c1bcb83785c2f538a75
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-17 18:39:45 +0600

    test(vr-page): added temporary virtual-reality page

---
+++ src/app/(pages)/vr/page.tsx
@@ -0,0 +1,13 @@
+import { DjHero, VrGames, VrSocialEvents } from "@/components/sections";
+
+const BarPage = () => {
+  return (
+    <>
+      <DjHero />
+      <VrGames />
+      <VrSocialEvents />
+    </>
+  );
+};
+
+export default BarPage;
