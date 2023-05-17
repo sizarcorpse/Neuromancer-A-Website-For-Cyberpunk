4e9396e3410ce8de4c142c568ba59ecb059960e0
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-17 18:33:55 +0600

    update(layout): added header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { Header } from "@/components/navigation";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "Neuromancer",
   description: "A Website For Cyberpunk",
@@ -14,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
