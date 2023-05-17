d0c3dea9c25c228a186da8f003251126db578ad1
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-17 18:32:34 +0600

    update(index): update index page

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,3 +1,10 @@
+export { default as DjFeatured } from "./DjFeatured";
+export { default as DjHero } from "./DjHero";
+export { default as DjWhy } from "./DjWhy";
+export { default as HomeBar } from "./HomeBar";
 export { default as HomeCorporate } from "./HomeCorporate";
 export { default as HomeExplore } from "./HomeExplore";
 export { default as HomeHero } from "./HomeHero";
+export { default as Reviews } from "./Reviews";
+export { default as VrGames } from "./VrGames";
+export { default as VrSocialEvents } from "./VrSocialEvents";
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,13 +1,16 @@
 export { default as NrButton } from "./NrButton";
+export { default as NrComponentCarousel } from "./NrComponentCarousel";
 export { default as NrComponentSlider } from "./NrComponentSlider";
 export { default as NrContainer } from "./NrContainer";
 export { default as NrDjCard } from "./NrDjCard";
 export { default as NrDropdown } from "./NrDropdown";
 export { default as NrEventCard } from "./NrEventCard";
+export { default as NrImageBox } from "./NrImageBox";
 export { default as NrImageSlider } from "./NrImageSlider";
 export { default as NrLinkButton } from "./NrLinkButton";
 export { default as NrList } from "./NrList";
 export { default as NrListItem } from "./NrListItem";
+export { default as NrReviewCard } from "./NrReviewCard";
 export { default as NrSocialCard } from "./NrSocialCard";
 export { default as NrText } from "./NrText";
 export { default as NrTitle } from "./NrTitle";
