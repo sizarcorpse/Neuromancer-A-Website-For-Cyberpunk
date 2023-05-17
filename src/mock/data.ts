33bb4054d1f1891d9fe995f4e6f39d13066ff03c
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-17 18:36:18 +0600

    update(mock): add new mock data

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -591,3 +591,492 @@ export const galleriesData = [
     width: 0,
   },
 ];
+
+export const djsData = [
+  {
+    name: "Excision",
+    description:
+      "Jeff Abel, better known by his stage name Excision, is a Canadian producer and disc jockey.",
+    image: {
+      src: "/assets/images/dj-8.jpg",
+      alt: "Excision",
+    },
+    social: {
+      facebook: "https://www.facebook.com/Excision/",
+      instagram: "https://www.instagram.com/excisionofficial/",
+      twitter: "https://twitter.com/excision",
+      soundcloud: "https://soundcloud.com/excision",
+    },
+  },
+  {
+    name: "Skrillex",
+    description:
+      "Sonny John Moore, known professionally as Skrillex, is an American DJ, record producer, musician, singer and songwriter.",
+    image: {
+      src: "/assets/images/dj-5.jpg",
+      alt: "Skrillex",
+    },
+    social: {
+      facebook: "https://www.facebook.com/skrillex",
+      instagram: "https://www.instagram.com/skrillex/",
+      twitter: "https://twitter.com/skrillex",
+      soundcloud: "https://soundcloud.com/skrillex",
+    },
+  },
+  {
+    name: "Diplo",
+    description:
+      "Thomas Wesley Pentz Jr., known professionally as Diplo, is an American DJ, songwriter, and record producer based in Los Angeles, California.",
+    image: {
+      src: "/assets/images/dj-6.jpg",
+      alt: "Diplo",
+    },
+    social: {
+      facebook: "https://www.facebook.com/diplo/",
+      instagram: "https://www.instagram.com/diplo/",
+      twitter: "https://twitter.com/diplo",
+      soundcloud: "https://soundcloud.com/diplo",
+    },
+  },
+  {
+    name: "Avicii",
+    description:
+      "Tim Bergling, known professionally as Avicii, was a Swedish DJ, electronic musician, and songwriter.",
+    image: {
+      src: "/assets/images/dj-7.jpg",
+      alt: "Avicii",
+    },
+    social: {
+      facebook: "https://www.facebook.com/avicii/",
+      instagram: "https://www.instagram.com/avicii/",
+      twitter: "https://twitter.com/avicii",
+      soundcloud: "https://soundcloud.com/aviciiofficial",
+    },
+  },
+];
+
+export const gameData = [
+  {
+    uuid: "1",
+    preset: "game",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/Vr9DCinemaIcon.svg",
+    title: "Mission Sigma 🔥",
+    status: "Escape Room",
+    description:
+      "The entire building has been turned into a tower filled with intricate traps and obstacles. You have been recommended as a specialist in such puzzles, and the secret service has helped you infiltrate the courtyard. The rest is up to you. Get past all the traps and shut down the nuclear...",
+    action: {
+      type: "button",
+      label: "Book To Play",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "2",
+    preset: "game",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrFreeRoamIcon.svg",
+    title: "Arizona Sunshine",
+    status: "Free Play",
+    description:
+      "Putting the undead back to rest is more thrilling than ever before - battle alongside 3 fellow survivors in the midst of a zombie apocalypse. Survive solo or in co-op, handle weapons with real-life movements, and explore a post-apocalyptic world in VR",
+    action: {
+      type: "button",
+      label: "Book To Play",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "3",
+    preset: "game",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrEscapeRoomIcon.svg",
+    title: "Quantum Arena",
+    status: "Free Roam",
+    description:
+      "Challenge your friends in an intense futuristic tournament! Team-up or fight in a free-for-all battle for the highest score. Beware as players are not the only danger to look out for!",
+    action: {
+      type: "button",
+      label: "Book To Play",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "4",
+    preset: "game",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrFreePlayIcon.svg",
+    title: "Jungle Quest ⚡",
+    status: "Escape Room",
+    description:
+      "During a walk in the park, you found a portal that leads to the mysterious world. An amazing sanctuary populated by animals appears in front of you. But how do you get back? \n To find the way home, you have to solve a series of puzzles and explore.",
+    action: {
+      type: "button",
+      label: "Book To Play",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "5",
+    preset: "game",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrPartyIcon.svg",
+    title: "Dynamite 🌠",
+    status: "",
+    description:
+      "Christmas is under threat! Santa has got caught in a blizzard, he has lost all the presents and cannot find his way home. \n Only you can save Christmas.",
+    action: {
+      type: "button",
+      label: "Book To Play",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+  {
+    uuid: "6",
+    preset: "game",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrDjIcon.svg",
+    title: "Chernobylite 💣",
+    status: "",
+    description:
+      "Chernobylite is a survival horror RPG set in a sci-fi open world. Uncover the disturbing secrets of Chernobyl in a 3D-scanned recreation of the real Exclusion Zone. \n It’s not about how you face your fears, but how you survive them.",
+    action: {
+      type: "button",
+      label: "Book To Play",
+      href: "/vr-free-play",
+    },
+    share: {
+      enabled: false,
+    },
+  },
+];
+
+export const socialEventData = [
+  {
+    uuid: "1",
+    preset: "social",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/Vr9DCinemaIcon.svg",
+    title: "Family Fun Night 🔥",
+    status: "",
+    description:
+      "Never miss family bonding time ever again. Come enjoy endless realities.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "VR Cinema 9D",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "All Ages Welcome",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: " Every Wednesday",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "Walk-ins Allowed",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Games, Drinks, Party",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Learn More",
+      href: "/vr-9d-cinema",
+    },
+    share: {
+      enabled: true,
+    },
+  },
+  {
+    uuid: "2",
+    preset: "social",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrFreeRoamIcon.svg",
+    title: "Disco Night 🎶",
+    status: "Coming Soon",
+    description:
+      "Have endless fun singing, drinking, playing and having a good laugh.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "Music, Games",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "All Ages Welcome",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "Every Thursday",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "Walk-ins Allowed",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Escape Room",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "Free Roam",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Learn More",
+      href: "/vr-free-roam",
+    },
+    share: {
+      enabled: true,
+    },
+  },
+  {
+    uuid: "3",
+    preset: "social",
+    galleries: [
+      {
+        src: "/assets/images/slider-image-3.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-1.jpg",
+        alt: "VR Cinema 9D",
+      },
+      {
+        src: "/assets/images/slider-image-2.jpg",
+        alt: "VR Cinema 9D",
+      },
+    ],
+    icon: "/assets/svg/VrEscapeRoomIcon.svg",
+    title: "DJ Friday 💣",
+    status: "",
+    description:
+      "Wrap your week off in a blast with DJ Friday - the ultimate adult playground.",
+    features: [
+      {
+        icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+        title: "DJs in Bar",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "All Ages Welcome",
+      },
+      {
+        icon: "/assets/svg/CalendarIcon.svg",
+        title: "All Ages Welcome",
+      },
+      {
+        icon: "/assets/svg/TicketIcon.svg",
+        title: "Starts @8 PM",
+      },
+      {
+        icon: "/assets/svg/GameDieIcon.svg",
+        title: "Music, Drinks, Party",
+      },
+    ],
+    action: {
+      type: "link",
+      label: "Join Event",
+      href: "/vr-escape-room",
+    },
+    share: {
+      enabled: true,
+    },
+  },
+];
+
+export const reviewsData = [
+  {
+    uuid: "1",
+    reviewer: "The Galactus",
+    avatar: "/assets/images/dj-1.jpg",
+    rating: 5,
+    review: "I had a blast. I will definitely be back again.",
+  },
+  {
+    uuid: "2",
+    reviewer: "Electro Groove",
+    avatar: "/assets/images/dj-2.jpg",
+    rating: 4,
+    review:
+      "The DJ played an incredible set, keeping the energy high throughout the night. The crowd was hyped, and the atmosphere was electric. I would have given it a perfect rating if the sound system was a bit better.",
+  },
+
+  {
+    uuid: "3",
+    reviewer: "Rhythm Master",
+    avatar: "/assets/images/dj-3.jpg",
+    rating: 3.5,
+    review:
+      "The DJ had some great tracks, but the transitions between songs were a bit rough. It affected the overall flow of the set. The venue was spacious, though, and the visuals were impressive.",
+  },
+
+  {
+    uuid: "4",
+    reviewer: "Beats Lover",
+    avatar: "/assets/images/dj-4.jpg",
+    rating: 5,
+    review:
+      "This DJ concert was mind-blowing! The DJ's music selection was on point, and the mixing was flawless. The crowd was in sync, and the energy was contagious. It was an unforgettable night that exceeded all my expectations.",
+  },
+  {
+    uuid: "5",
+    reviewer: "VirtualAdventurer",
+    avatar: "/assets/images/dj-5.jpg",
+    rating: 5,
+    review:
+      "I was blown away by the VR games at this place! The graphics were incredibly realistic, and the gameplay was immersive. The staff was helpful and made sure I had the best experience possible. Can't wait to go back and try more games!",
+  },
+
+  {
+    uuid: "6",
+    reviewer: "TechEnthusiast",
+    avatar: "/assets/images/dj-6.jpg",
+    rating: 4.5,
+    review:
+      "The VR experience was top-notch. The equipment was modern and comfortable to use. The game selection was diverse, catering to different interests. The only downside was that some of the games had long wait times, but it was worth it once I got my turn.",
+  },
+
+  {
+    uuid: "7",
+    reviewer: "GamingGuru",
+    avatar: "/assets/images/dj-7.jpg",
+    rating: 3,
+    review:
+      "The VR games were enjoyable, but the overall experience fell short of my expectations. The tracking was occasionally glitchy, which affected gameplay. The staff was friendly, but they seemed overwhelmed with the number of customers. It has potential, but improvements are needed.",
+  },
+
+  {
+    uuid: "8",
+    reviewer: "AdrenalineJunkie",
+    avatar: "/assets/images/dj-8.jpg",
+    rating: 5,
+    review:
+      "If you're an adrenaline junkie like me, you'll love this VR experience! The games were intense, and I felt like I was actually in the virtual world. The equipment was top-of-the-line, and the staff was passionate about VR. I highly recommend trying out their thrilling VR games!",
+  },
+];
