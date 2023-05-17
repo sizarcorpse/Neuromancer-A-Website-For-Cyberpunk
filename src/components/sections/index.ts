31f740aca3779c4f183ea1f81e930e7f5c5e2de9
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-17 18:46:36 +0600

    update(index): update index page

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,7 +1,6 @@
 export { default as DjFeatured } from "./DjFeatured";
 export { default as DjHero } from "./DjHero";
 export { default as DjWhy } from "./DjWhy";
-export { default as HomeBar } from "./HomeBar";
 export { default as HomeCorporate } from "./HomeCorporate";
 export { default as HomeExplore } from "./HomeExplore";
 export { default as HomeHero } from "./HomeHero";
