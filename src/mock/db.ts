abe4fda0ec127374b095e53f5224db73899ab548
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-16 02:24:07 +0600

    add(db): new mock data

---
+++ src/mock/db.ts
@@ -0,0 +1,349 @@
+export const socials = {
+  facebook: "https://www.facebook.com/neuromancer.vr",
+  instagram: "https://www.instagram.com/neuromancer.vr/",
+  twitter: "https://twitter.com/neuromancer_vr",
+  soundcloud: "https://soundcloud.com/neuromancer-vr",
+};
+
+export const header = {
+  dropdown: {
+    featuredEvents: [
+      {
+        uuid: "1",
+        preset: "vr",
+        galleries: [
+          {
+            src: "/assets/images/slider-image-1.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-2.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-3.jpg",
+            alt: "VR Cinema 9D",
+          },
+        ],
+        icon: "/assets/svg/Vr9DCinemaIcon.svg",
+        title: "VR 9D Cinema 🔥",
+        status: "",
+        description:
+          "Experience boundless virtual worlds and events with real-time effects.",
+        features: [
+          {
+            icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+            title: "Starts $5",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "5 Years and Up",
+          },
+          {
+            icon: "/assets/svg/CalendarIcon.svg",
+            title: "4 to 8 Minutes",
+          },
+          {
+            icon: "/assets/svg/TicketIcon.svg",
+            title: "In Events",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "Multiplayer",
+          },
+        ],
+        action: {
+          type: "link",
+          label: "Join Event",
+          href: "/vr-9d-cinema",
+        },
+        share: {
+          enabled: false,
+        },
+      },
+      {
+        uuid: "2",
+        preset: "vr",
+        galleries: [
+          {
+            src: "/assets/images/slider-image-3.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-1.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-2.jpg",
+            alt: "VR Cinema 9D",
+          },
+        ],
+        icon: "/assets/svg/VrFreeRoamIcon.svg",
+        title: "VR Free Roam",
+        status: "",
+        description:
+          "Go completely wireless and soul your VR gaming capabilities.",
+        features: [
+          {
+            icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+            title: "Starts $5",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "5 Years and Up",
+          },
+          {
+            icon: "/assets/svg/CalendarIcon.svg",
+            title: "4 to 8 Minutes",
+          },
+          {
+            icon: "/assets/svg/TicketIcon.svg",
+            title: "In Events",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "Multiplayer",
+          },
+        ],
+        action: {
+          type: "link",
+          label: "Join Event",
+          href: "/vr-free-roam",
+        },
+        share: {
+          enabled: false,
+        },
+      },
+      {
+        uuid: "3",
+        preset: "vr",
+        galleries: [
+          {
+            src: "/assets/images/slider-image-3.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-1.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-2.jpg",
+            alt: "VR Cinema 9D",
+          },
+        ],
+        icon: "/assets/svg/VrEscapeRoomIcon.svg",
+        title: "VR Escape Room",
+        status: "",
+        description:
+          "Enter a puzzle-solving adventure with your friends and family.",
+        features: [
+          {
+            icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+            title: "Starts $5",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "5 Years and Up",
+          },
+          {
+            icon: "/assets/svg/CalendarIcon.svg",
+            title: "4 to 8 Minutes",
+          },
+          {
+            icon: "/assets/svg/TicketIcon.svg",
+            title: "In Events",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "Multiplayer",
+          },
+        ],
+        action: {
+          type: "link",
+          label: "Join Event",
+          href: "/vr-escape-room",
+        },
+        share: {
+          enabled: false,
+        },
+      },
+      {
+        uuid: "4",
+        preset: "vr",
+        galleries: [
+          {
+            src: "/assets/images/slider-image-3.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-1.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-2.jpg",
+            alt: "VR Cinema 9D",
+          },
+        ],
+        icon: "/assets/svg/VrFreePlayIcon.svg",
+        title: "VR Free Play ⚡",
+        status: "",
+        description:
+          "Play over 70 engaging and exciting games that offer a unique type of fun.",
+        features: [
+          {
+            icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+            title: "Starts $5",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "5 Years and Up",
+          },
+          {
+            icon: "/assets/svg/CalendarIcon.svg",
+            title: "4 to 8 Minutes",
+          },
+          {
+            icon: "/assets/svg/TicketIcon.svg",
+            title: "In Events",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "Multiplayer",
+          },
+        ],
+        action: {
+          type: "link",
+          label: "Join Event",
+          href: "/vr-free-play",
+        },
+        share: {
+          enabled: false,
+        },
+      },
+      {
+        uuid: "5",
+        preset: "vr",
+        galleries: [
+          {
+            src: "/assets/images/slider-image-2.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-3.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-1.jpg",
+            alt: "VR Cinema 9D",
+          },
+        ],
+        icon: "/assets/svg/VrPartyIcon.svg",
+        title: "VR Party 🌠",
+        status: "",
+        description: "Great for birthdays, family gatherings, hangouts.",
+        features: [
+          {
+            icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+            title: "Starts $5",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "5 Years and Up",
+          },
+          {
+            icon: "/assets/svg/CalendarIcon.svg",
+            title: "4 to 8 Minutes",
+          },
+          {
+            icon: "/assets/svg/TicketIcon.svg",
+            title: "In Events",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "Multiplayer",
+          },
+        ],
+        action: {
+          type: "link",
+          label: "Join Event",
+          href: "/vr-free-play",
+        },
+        share: {
+          enabled: false,
+        },
+      },
+      {
+        uuid: "6",
+        preset: "vr",
+        galleries: [
+          {
+            src: "/assets/images/slider-image-3.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-1.jpg",
+            alt: "VR Cinema 9D",
+          },
+          {
+            src: "/assets/images/slider-image-2.jpg",
+            alt: "VR Cinema 9D",
+          },
+        ],
+        icon: "/assets/svg/VrDjIcon.svg",
+        title: "Bar 💄",
+        status: "",
+        description:
+          "Enjoy traditional beers, cocktails, and snacks in the heart of fun attractions.",
+        features: [
+          {
+            icon: "/assets/svg/JigsawPuzzlePieceIcon.svg",
+            title: "Starts $5",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "5 Years and Up",
+          },
+          {
+            icon: "/assets/svg/CalendarIcon.svg",
+            title: "4 to 8 Minutes",
+          },
+          {
+            icon: "/assets/svg/TicketIcon.svg",
+            title: "In Events",
+          },
+          {
+            icon: "/assets/svg/GameDieIcon.svg",
+            title: "Multiplayer",
+          },
+        ],
+        action: {
+          type: "link",
+          label: "Join Event",
+          href: "/vr-free-play",
+        },
+        share: {
+          enabled: false,
+        },
+      },
+    ],
+    socials: socials,
+    actionLink: "/explore",
+    styles: {
+      footer: true,
+    },
+  },
+  navigation: [
+    {
+      id: "1",
+      order: 1,
+      label: "Festival",
+      href: "/dj",
+    },
+    {
+      id: "2",
+      order: 2,
+      label: "Virtual Reality",
+      href: "/vr",
+    },
+  ],
+};
