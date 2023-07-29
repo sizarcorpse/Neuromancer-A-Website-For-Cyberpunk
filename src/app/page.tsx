8ceb80e2501dc17071507d8acfb93926b234546e
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-29 23:48:57 +0600

    add(home-page): homepage completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,18 @@ import {
   Newsletter,
   Reviews,
 } from "@/components/sections";
+
+import { home, socials } from "@/mock/db";
+
 const Home = () => {
+  const { hero, explore, corporate, reviews, newsletter } = home;
   return (
     <>
-      <HomeHero />
-      <HomeExplore />
-      <HomeCorporate />
-      <Reviews />
-      <Newsletter />
+      <HomeHero hero={hero} />
+      <HomeExplore explore={explore as any} />
+      <HomeCorporate corporate={corporate as any} />
+      <Reviews reviews={reviews} />
+      <Newsletter newsletter={newsletter} socials={socials} />
     </>
   );
 };
