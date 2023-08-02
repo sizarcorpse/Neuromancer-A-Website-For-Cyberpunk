6a07aa5d9783a9d7330fbe0a33051670e7fd191f
Sizar Corpse <sizarcorpse@gmail.com>
2023-08-02 22:58:56 +0600

    refactor(section): added interface, fix css

--- src/components/sections/DjFeatured.tsx
+++ src/components/sections/DjFeatured.tsx
@@ -6,11 +6,32 @@ import {
   NrText,
   NrTitle,
 } from "@/components/ui";
-import { djsData } from "@/mock/data";
-import { Dj, DjCard, Djs } from "@/types/dj";
-const DjFeatured = () => {
+
+import { Djs } from "@/types/dj";
+import { FC } from "react";
+
+interface DjFeaturedProp {
+  dj: {
+    images?: {};
+    content: {
+      title: string;
+      description: string;
+      djs: Djs;
+      primaryAction: {
+        label: string;
+        type: string;
+        href: string;
+      };
+    };
+  };
+}
+
+const DjFeatured: FC<DjFeaturedProp> = ({ dj }) => {
+  const {
+    content: { title, description, djs, primaryAction },
+  } = dj;
   return (
-    <section className="bg-darker">
+    <section className="bg-nr-darker bg-[linear-gradient(180deg,rgba(42,15,15,0)_50%,rgba(9,9,11,1)_100%),linear-gradient(0deg,rgba(42,15,15,0)_50%,rgba(9,9,11,1)_100%),url('/assets/media/festival-explore-background-pattern.png')] bg-no-repeat bg-center bg-cover">
       <NrContainer
         styles={{
           size: "md",
@@ -24,20 +45,18 @@ const DjFeatured = () => {
               align: "center",
             }}
           >
-            Featured DJs
+            {title}
           </NrTitle>
           <NrText
             styles={{
               align: "center",
             }}
           >
-            From dance hall to hip-hop and everything in between, our Featured
-            DJs are the crème de la crème of the local music scene, ready to get
-            you moving.
+            {description}
           </NrText>
         </div>
-        <div className="hidden m-auto md:flex flex-col flex-wrap justify-start items-stretch gap-4 md:flex-row">
-          {djsData.map((item, index) => (
+        <div className="hidden m-auto md:flex flex-col flex-wrap justify-between items-stretch gap-4 md:flex-row">
+          {djs.map((item, index) => (
             <div
               key={index}
               className={`grow-0 shrink justify-start md:basis-column-3-gap-4 lg:basis-column-4-gap-4`}
@@ -49,14 +68,14 @@ const DjFeatured = () => {
         {/* note : in order to make slider component center at mobile view,
         use the used `max-w` value on `NrComponent` */}
         <div className="block w-auto max-w-xs md:hidden">
-          <NrComponentSlider item={djsData as Djs} component="NrDjCard" />
+          <NrComponentSlider item={djs} component="NrDjCard" />
         </div>
         <NrButton
           styles={{
             icon: true,
           }}
         >
-          Explore DJs
+          {primaryAction.label}
         </NrButton>
       </NrContainer>
     </section>
